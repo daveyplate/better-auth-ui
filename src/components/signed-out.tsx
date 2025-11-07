0a0f2683ebb47b903cf7bbb3a94bfef3b1d78abf
Picard P. <59894191+picardplaisimond@users.noreply.github.com>
2025-11-06 22:17:33 -0500

    Add option to treat pending session as "signed out" on <SignedOut />
    A boolean that indicates whether to treat pending sessions
    as signed out. Defaults to false.

--- src/components/signed-out.tsx
+++ src/components/signed-out.tsx
@@ -10,11 +10,14 @@ import { AuthUIContext } from "../lib/auth-ui-provider"
  * state is not pending. If a session exists or is being loaded, nothing is rendered.
  * Useful for displaying sign-in prompts or content exclusive to guests.
  */
-export function SignedOut({ children }: { children: ReactNode }) {
+export function SignedOut({ children, treatPendingAsSignedOut=false }: { children: ReactNode, treatPendingAsSignedOut: boolean }) {
     const {
         hooks: { useSession }
     } = useContext(AuthUIContext)
-    const { data, isPending } = useSession()
+    const { data, isPending } = useSession();
+
+    if(treatPendingAsSignedOut)
+        return !data || isPending ? children : null
 
     return !data && !isPending ? children : null
 }
