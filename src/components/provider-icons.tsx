cfa390438330190be8b1b15d02425df8d2f7b01a
Shridhar Deshmukh <shridhar@neon.tech>
2025-12-09 04:58:58 +0100

    feat: add Vercel in social providers

--- src/components/provider-icons.tsx
+++ src/components/provider-icons.tsx
@@ -343,6 +343,32 @@ export const TwitchIcon: ProviderIcon = ({ className }) => (
     </svg>
 )
 
+export const VercelIcon: ProviderIcon = ({ className }) => (
+    <svg
+        width="46"
+        height="46"
+        viewBox="0 0 46 46"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <g clip-path="url(#clip0_618_288)">
+            <path
+                d="M23 46C35.7026 46 46 35.7026 46 23C46 10.2975 35.7026 0 23 0C10.2975 0 0 10.2975 0 23C0 35.7026 10.2975 46 23 46Z"
+                fill="white"
+            />
+            <path
+                d="M34.3977 30.5223H11.6022C15.41 23.8561 19.1781 17.2615 22.9987 10.5749C26.8077 17.2398 30.5823 23.8446 34.3977 30.5223Z"
+                fill="black"
+            />
+        </g>
+        <defs>
+            <clipPath id="clip0_618_288">
+                <rect width="46" height="46" fill="white" />
+            </clipPath>
+        </defs>
+    </svg>
+)
+
 export const VKIcon: ProviderIcon = ({ className }) => (
     <svg
         className={className}
